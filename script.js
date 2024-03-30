36bac76d0f9ba7a79ee6a40a48de39a2e1d6f387
Irbaz2004 <irbazahmeds20@gmail.com>
2024-03-30 08:00:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    setTimeout(function(){
+        var loader = document.getElementById("load");
+        loader.style.display = "none";
+        window.location.href = 'index1.html';
+    },5000)
+}
